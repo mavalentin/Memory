a81b67821dac37c439ef22faf68c347f1b4c401a
Manuel Valentin <manu.valentin.95@gmail.com>
2017-03-22 18:22:25 +0100

    12 and firefox ok

--- memory-game/js/index.js
+++ memory-game/js/index.js
@@ -147,36 +147,6 @@
 			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
 			id: 6
 		},
-		{
-			name: "photoshop",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
-			id: 7
-		},
-		{
-			name: "python",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
-			id: 8
-		},
-		{
-			name: "rails",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
-			id: 9
-		},
-		{
-			name: "sass",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
-			id: 10
-		},
-		{
-			name: "sublime",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
-			id: 11
-		},
-		{
-			name: "wordpress",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
-			id: 12
-		},
 	];
     
 	Memory.init(cards);
